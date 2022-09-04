980eec9e78cd90e93322084b6ce7677ae9c1cd73
akhtarraja513 <akhtarraja513@gmail.com>
2022-09-04 14:11:32 +0530

    Final Commit Done

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,20 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const Display = (event) => {
+    // console.log(event);
+    if(event.target.id === "click") {
+      // console.log(event.target.value)
+      // event.target.id = "para"
+      setShow(true);
+    }
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      {show && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      <button onClick={(e) => Display(e)} id="click">Click</button>
     </div>
   );
 }
